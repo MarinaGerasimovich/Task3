9307a1b457d718eba99922da00fc7f0e67a136c2
MarinaGerasimovich <marina_marina_gerasimovich@mail.ru>
2024-03-15 23:03:37 +0300

    1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,29 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+    @Autowired
+    private Timer timer;
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        //System.out.println(new Timer().getTime());
+        // он каждый раз создает новый объект класса Timer
+        // и будет возвращать разное время, а нам нужно одинаковое
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
